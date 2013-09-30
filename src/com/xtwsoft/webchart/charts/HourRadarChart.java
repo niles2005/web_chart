a19344e522fba3a2a2720b1624b186f808790a5f
zoboya <rayzang8@gmail.com>
2013-09-30 11:07:48 +0800

    update

--- src/com/xtwsoft/webchart/charts/HourRadarChart.java
+++ src/com/xtwsoft/webchart/charts/HourRadarChart.java
@@ -2,6 +2,7 @@ package com.xtwsoft.webchart.charts;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.geom.Point2D;
@@ -10,26 +11,17 @@ import com.alibaba.fastjson.JSONObject;
 
 public class HourRadarChart extends BaseChart {
 	private int m_coordinateRadius ; 
-	private static final int m_marginHeight = 18;
-	private static final int m_marginWidth = 40;
-	private static final float dash1[] = {1.2f};
-	private static final BasicStroke dashed = new BasicStroke(0.5f,
-            BasicStroke.CAP_BUTT,
-            BasicStroke.JOIN_MITER,
-            10.0f, dash1, 0.0f);
+	private static final int m_marginHeight = 16;
+	private static final int m_marginWidth = 30;
 	
 	public HourRadarChart(JSONObject element,JSONObject legend,int imageWidth,int imageHeight) {
 		super(element,legend,imageWidth,imageHeight);
-		
-	
 	}
 	
 	
 	public void draw(Graphics2D g2) {
 		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		g2.setColor(Color.BLACK);
-		
-		
 		Point2D.Double centerPoint = new Point2D.Double(m_imageWidth/2,m_imageHeight - m_marginHeight);
 		g2.translate(centerPoint.x,centerPoint.y);
 		
@@ -41,14 +33,31 @@ public class HourRadarChart extends BaseChart {
 			m_coordinateRadius = shapeMaxWidth;
 		}
 		
+		int calibration = 7;	
+		int gridGap = m_coordinateRadius / calibration;
 		
+		drawGrid(g2 ,gridGap,calibration);
+		drawAxis(g2);
+		drawMark(g2);
+		drawValue(g2 , gridGap );
 		
-		int gridGap = m_coordinateRadius / 6;
-		for (int i = 0; i < 6; i++) {
+		g2.translate(-centerPoint.x,-centerPoint.y);
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
+	}
+	
+	
+	private void drawGrid(Graphics2D g2 ,int gridGap ,int calibration){
+		float dash1[] = {1.2f};
+		BasicStroke dashed = new BasicStroke(0.5f,
+	            BasicStroke.CAP_BUTT,
+	            BasicStroke.JOIN_MITER,
+	            10.0f, dash1, 0.0f);
+		
+		for (int i = 0; i < calibration; i++) {
 			int currentRadius = m_coordinateRadius - i * gridGap;
 			
-			if( i % 2 == 0){
-				g2.setColor(new Color(0,0,0,10));
+			if( i % 2 == 1){
+				g2.setColor(new Color(0,0,0,6));
 				g2.fillArc(-currentRadius, -currentRadius,currentRadius * 2, currentRadius * 2, 0, 180);
 				g2.setColor(this.m_backgroundColor);
 				g2.fillArc(-currentRadius+gridGap, -currentRadius+gridGap,(currentRadius-gridGap) * 2, (currentRadius-gridGap) * 2, 0, 180);
@@ -58,35 +67,51 @@ public class HourRadarChart extends BaseChart {
 			g2.drawArc(-currentRadius, -currentRadius,currentRadius * 2, currentRadius * 2, 0, 180);
 		}
 		
+	}
+	
+	
+	private void drawAxis(Graphics2D g2){
 		g2.setStroke(new BasicStroke(1));
-		g2.setColor(Color.LIGHT_GRAY);
-		
-		int degreeGap = 180 / 6;
-		int time = 50;
-		for (int i = 0; i < 6; i++) {
+		int degreeGap = 180 / 12;
+		for (int i = 0; i < 12; i++) {
 			g2.rotate(Math.toRadians(-degreeGap));
+			if (i % 2 == 0) {
+				g2.setColor(new Color(240,240,240));
+			} else {
+				g2.setColor(new Color(225,225,225));
+			}
 			g2.drawLine(0, 0, m_coordinateRadius, 0);
 		}
 		g2.rotate(Math.toRadians(180));
-		
-		g2.setColor(Color.GRAY);
+		g2.setColor(new Color(230,230,230));
 		g2.drawLine(0, 0, m_coordinateRadius, 0);
+	}
+	
+	private void drawMark(Graphics2D g2){
+		int degreeGap = 180 / 6;
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
+		g2.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 10));
+		g2.setColor(Color.GRAY);
+		int time = 50;
 		g2.drawString("8:00", m_coordinateRadius + 4, 0);
 		for (int i = 0; i < 6; i++) {
 			int currentTime = time - i * 10;
 			int currentAngle = (i + 1)* degreeGap;
 			String currentTimeStr = "7:"+ currentTime;
+			if (currentTime == 0){
+				currentTimeStr += "0";
+			}
 			double pointX = Math.cos(Math.toRadians(-currentAngle)) * (m_coordinateRadius + 4) ;
 			double pointY = Math.sin(Math.toRadians(-currentAngle)) * (m_coordinateRadius + 4) ;
-//			double offsetX =  Math.cos(Math.toRadians(-currentAngle)) * 20;
-//			double offsetY =  Math.sin(Math.toRadians(-currentAngle)) * 20;
+			double offsetX =  (1-Math.cos(Math.toRadians(-currentAngle)) ) * 10;
 			
-			g2.drawString(currentTimeStr, (int)pointX , (int)pointY );
+			g2.drawString(currentTimeStr, (int)(pointX - offsetX) , (int)pointY );
 			
 		}
-		
-		
-		
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+	}
+	
+	private void drawValue(Graphics2D g2 , int gridGap){
 		g2.setStroke(new BasicStroke(8, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
 		for (int i = 0; i < 6; i++) {
 			Color xcolor = new Color( (int)(Math.random()*255) ,(int)(Math.random()*255) ,(int)(Math.random()*255));
@@ -113,21 +138,8 @@ public class HourRadarChart extends BaseChart {
 			g2.fillOval((int)pointX -3 , (int)pointY -3 , 6, 6);
 			
 		}
-		
-		
-		
-		
-		
-		
-		g2.translate(-centerPoint.x,-centerPoint.y);
-		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
 	}
-	
-	private void drawGrid(Graphics2D g2){
 		
-		
-	}
-	
 	
 
 }
